9896b649854870dbd418a67348e53af07206f9dd
syzymon <s.tworkowski@student.uw.edu.pl>
2021-11-21 20:10:18 +0100

    small fix + reformat

--- hourglass_transformer_pytorch/hourglass_transformer_pytorch.py
+++ hourglass_transformer_pytorch/hourglass_transformer_pytorch.py
@@ -2,45 +2,57 @@ import torch
 from torch import nn, einsum
 import torch.nn.functional as F
 from einops import rearrange, reduce, repeat
+import math
 
 # helpers
+
 
 def exists(val):
     return val is not None
 
+
 def default(val, d):
     return val if exists(val) else d
 
-def pad_to_multiple(tensor, multiple, dim = -1, value = 0):
+
+def pad_to_multiple(tensor, multiple, dim=-1, value=0):
     seq_len = tensor.shape[dim]
     m = seq_len / multiple
     if m.is_integer():
         return tensor
     remainder = math.ceil(m) * multiple - seq_len
     pad_offset = (0,) * (-1 - dim) * 2
-    return F.pad(tensor, (*pad_offset, 0, remainder), value = value)
+    return F.pad(tensor, (*pad_offset, 0, remainder), value=value)
 
-def cast_tuple(val, depth = 1):
+
+def cast_tuple(val, depth=1):
     return val if isinstance(val, tuple) else ((val,) * depth)
+
 
 # factory
 
 def get_hourglass_transformer(
-    dim,
-    *,
-    depth,
-    shorten_factor,
-    attn_resampling,
-    updown_sample_type,
-    **kwargs
+        dim,
+        *,
+        depth,
+        shorten_factor,
+        attn_resampling,
+        updown_sample_type,
+        **kwargs
 ):
-    assert isinstance(depth, int) or (isinstance(depth, tuple)  and len(depth) == 3), 'depth must be either an integer or a tuple of 3, indicating (pre_transformer_depth, <nested-hour-glass-config>, post_transformer_depth)'
-    assert not (isinstance(depth, int) and shorten_factor), 'there does not need to be a shortening factor when only a single transformer block is indicated (depth of one integer value)'
+    assert isinstance(depth, int) or (isinstance(depth, tuple) and len(
+        depth) == 3), 'depth must be either an integer or a tuple of 3, indicating (pre_transformer_depth, <nested-hour-glass-config>, post_transformer_depth)'
+    assert not (isinstance(depth,
+                           int) and shorten_factor), 'there does not need to be a shortening factor when only a single transformer block is indicated (depth of one integer value)'
 
     if isinstance(depth, int):
-        return Transformer(dim = dim, depth = depth, **kwargs)
+        return Transformer(dim=dim, depth=depth, **kwargs)
 
-    return HourglassTransformer(dim = dim, depth = depth, shorten_factor = shorten_factor, attn_resampling = attn_resampling, updown_sample_type = updown_sample_type, **kwargs)
+    return HourglassTransformer(dim=dim, depth=depth,
+                                shorten_factor=shorten_factor,
+                                attn_resampling=attn_resampling,
+                                updown_sample_type=updown_sample_type, **kwargs)
+
 
 # up and down sample classes
 
@@ -50,7 +62,8 @@ class NaiveDownsample(nn.Module):
         self.shorten_factor = shorten_factor
 
     def forward(self, x):
-        return reduce(x, 'b (n s) d -> b n d', 'mean', s = self.shorten_factor)
+        return reduce(x, 'b (n s) d -> b n d', 'mean', s=self.shorten_factor)
+
 
 class NaiveUpsample(nn.Module):
     def __init__(self, shorten_factor):
@@ -58,7 +71,8 @@ class NaiveUpsample(nn.Module):
         self.shorten_factor = shorten_factor
 
     def forward(self, x):
-        return repeat(x, 'b n d -> b (n s) d', s = self.shorten_factor)
+        return repeat(x, 'b n d -> b (n s) d', s=self.shorten_factor)
+
 
 class LinearDownsample(nn.Module):
     def __init__(self, dim, shorten_factor):
@@ -67,8 +81,9 @@ class LinearDownsample(nn.Module):
         self.shorten_factor = shorten_factor
 
     def forward(self, x):
-        x = rearrange(x, 'b (n s) d -> b n (s d)', s = self.shorten_factor)
+        x = rearrange(x, 'b (n s) d -> b n (s d)', s=self.shorten_factor)
         return self.proj(x)
+
 
 class LinearUpsample(nn.Module):
     def __init__(self, dim, shorten_factor):
@@ -78,7 +93,8 @@ class LinearUpsample(nn.Module):
 
     def forward(self, x):
         x = self.proj(x)
-        return rearrange(x, 'b n (s d) -> b (n s) d', s = self.shorten_factor)
+        return rearrange(x, 'b n (s d) -> b (n s) d', s=self.shorten_factor)
+
 
 # classes
 
@@ -91,14 +107,15 @@ class PreNormResidual(nn.Module):
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs) + x
 
+
 class Attention(nn.Module):
     def __init__(
-        self,
-        dim,
-        heads = 8,
-        dim_head = 64,
-        dropout = 0.,
-        causal = False
+            self,
+            dim,
+            heads=8,
+            dim_head=64,
+            dropout=0.,
+            causal=False
     ):
         super().__init__()
         self.heads = heads
@@ -106,18 +123,19 @@ class Attention(nn.Module):
         self.scale = dim_head ** -0.5
         inner_dim = heads * dim_head
 
-        self.to_q = nn.Linear(dim, inner_dim, bias = False)
-        self.to_kv = nn.Linear(dim, inner_dim * 2, bias = False)
+        self.to_q = nn.Linear(dim, inner_dim, bias=False)
+        self.to_kv = nn.Linear(dim, inner_dim * 2, bias=False)
         self.to_out = nn.Linear(inner_dim, dim)
 
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x, context = None, mask = None):
+    def forward(self, x, context=None, mask=None):
         h, device = self.heads, x.device
         kv_input = default(context, x)
 
-        q, k, v = self.to_q(x), *self.to_kv(kv_input).chunk(2, dim = -1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (q, k, v))
+        q, k, v = self.to_q(x), *self.to_kv(kv_input).chunk(2, dim=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h),
+                      (q, k, v))
 
         q = q * self.scale
 
@@ -130,18 +148,20 @@ class Attention(nn.Module):
 
         if self.causal:
             i, j = sim.shape[-2:]
-            mask = torch.ones(i, j, device = device, dtype = torch.bool).triu_(j - i + 1)
+            mask = torch.ones(i, j, device=device, dtype=torch.bool).triu_(
+                j - i + 1)
             mask = rearrange(mask, 'i j -> () () i j')
             sim = sim.masked_fill(mask, mask_value)
 
-        attn = sim.softmax(dim = -1)
+        attn = sim.softmax(dim=-1)
         attn = self.dropout(attn)
 
         out = einsum('b h i j, b h j d -> b h i d', attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)', h = h)
+        out = rearrange(out, 'b h n d -> b n (h d)', h=h)
         return self.to_out(out)
 
-def FeedForward(dim, mult = 4, dropout = 0.):
+
+def FeedForward(dim, mult=4, dropout=0.):
     return nn.Sequential(
         nn.Linear(dim, dim * mult),
         nn.GELU(),
@@ -149,57 +169,63 @@ def FeedForward(dim, mult = 4, dropout = 0.):
         nn.Linear(dim * mult, dim)
     )
 
+
 # transformer classes
 
 class Transformer(nn.Module):
     def __init__(
-        self,
-        dim,
-        *,
-        depth,
-        causal = False,
-        heads = 8,
-        dim_head = 64,
-        attn_dropout = 0.,
-        ff_mult = 4,
-        ff_dropout = 0.,
-        norm_out = False
+            self,
+            dim,
+            *,
+            depth,
+            causal=False,
+            heads=8,
+            dim_head=64,
+            attn_dropout=0.,
+            ff_mult=4,
+            ff_dropout=0.,
+            norm_out=False
     ):
         super().__init__()
         self.layers = nn.ModuleList([])
 
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                PreNormResidual(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = attn_dropout, causal = causal)),
-                PreNormResidual(dim, FeedForward(dim, mult = ff_mult, dropout = ff_dropout))
+                PreNormResidual(dim,
+                                Attention(dim, heads=heads, dim_head=dim_head,
+                                          dropout=attn_dropout, causal=causal)),
+                PreNormResidual(dim, FeedForward(dim, mult=ff_mult,
+                                                 dropout=ff_dropout))
             ]))
 
         self.norm = nn.LayerNorm(dim) if norm_out else nn.Identity()
 
-    def forward(self, x, context = None, mask = None):
+    def forward(self, x, context=None, mask=None):
         for attn, ff in self.layers:
-            x = attn(x, context = context, mask = mask)
+            x = attn(x, context=context, mask=mask)
             x = ff(x)
 
         return self.norm(x)
 
+
 class HourglassTransformer(nn.Module):
     def __init__(
-        self,
-        dim,
-        *,
-        depth,
-        shorten_factor = 2,
-        attn_resampling = True,
-        updown_sample_type = 'naive',
-        heads = 8,
-        dim_head = 64,
-        causal = False,
-        norm_out = False
+            self,
+            dim,
+            *,
+            depth,
+            shorten_factor=2,
+            attn_resampling=True,
+            updown_sample_type='naive',
+            heads=8,
+            dim_head=64,
+            causal=False,
+            norm_out=False
     ):
         super().__init__()
         assert len(depth) == 3, 'depth should be a tuple of length 3'
-        assert updown_sample_type in {'naive', 'linear'}, 'downsample / upsample type must be either naive (average pool and repeat) or linear (linear projection and reshape)'
+        assert updown_sample_type in {'naive',
+                                      'linear'}, 'downsample / upsample type must be either naive (average pool and repeat) or linear (linear projection and reshape)'
 
         pre_layers_depth, valley_depth, post_layers_depth = depth
 
@@ -211,9 +237,9 @@ class HourglassTransformer(nn.Module):
             shorten_factor, rest_shorten_factor = shorten_factor, shorten_factor
 
         transformer_kwargs = dict(
-            dim = dim,
-            heads = heads,
-            dim_head = dim_head
+            dim=dim,
+            heads=heads,
+            dim_head=dim_head
         )
 
         self.causal = causal
@@ -221,44 +247,49 @@ class HourglassTransformer(nn.Module):
 
         if updown_sample_type == 'naive':
             self.downsample = NaiveDownsample(shorten_factor)
-            self.upsample   = NaiveUpsample(shorten_factor)
+            self.upsample = NaiveUpsample(shorten_factor)
         elif updown_sample_type == 'linear':
             self.downsample = LinearDownsample(dim, shorten_factor)
-            self.upsample   = LinearUpsample(dim, shorten_factor)
+            self.upsample = LinearUpsample(dim, shorten_factor)
         else:
-            raise ValueError(f'unknown updown_sample_type keyword value - must be either naive or linear for now')
+            raise ValueError(
+                f'unknown updown_sample_type keyword value - must be either naive or linear for now')
 
         self.valley_transformer = get_hourglass_transformer(
-            shorten_factor = rest_shorten_factor,
-            depth = valley_depth,
-            attn_resampling = attn_resampling,
-            updown_sample_type = updown_sample_type,
-            causal = causal,
+            shorten_factor=rest_shorten_factor,
+            depth=valley_depth,
+            attn_resampling=attn_resampling,
+            updown_sample_type=updown_sample_type,
+            causal=causal,
             **transformer_kwargs
         )
 
-        self.attn_resampling_pre_valley = Transformer(depth = 1, **transformer_kwargs) if attn_resampling else None
-        self.attn_resampling_post_valley = Transformer(depth = 1, **transformer_kwargs) if attn_resampling else None
+        self.attn_resampling_pre_valley = Transformer(depth=1,
+                                                      **transformer_kwargs) if attn_resampling else None
+        self.attn_resampling_post_valley = Transformer(depth=1,
+                                                       **transformer_kwargs) if attn_resampling else None
 
-        self.pre_transformer = Transformer(depth = pre_layers_depth, causal = causal, **transformer_kwargs)
-        self.post_transformer = Transformer(depth = post_layers_depth, causal = causal, **transformer_kwargs)
+        self.pre_transformer = Transformer(depth=pre_layers_depth,
+                                           causal=causal, **transformer_kwargs)
+        self.post_transformer = Transformer(depth=post_layers_depth,
+                                            causal=causal, **transformer_kwargs)
         self.norm_out = nn.LayerNorm(dim) if norm_out else nn.Identity()
 
-    def forward(self, x, mask = None):
+    def forward(self, x, mask=None):
         # b : batch, n : sequence length, d : feature dimension, s : shortening factor
 
         s, b, n = self.shorten_factor, *x.shape[:2]
 
         # top half of hourglass, pre-transformer layers
 
-        x = self.pre_transformer(x, mask = mask)
+        x = self.pre_transformer(x, mask=mask)
 
         # pad to multiple of shortening factor, in preparation for pooling
 
-        x = pad_to_multiple(x, s, dim = -2)
+        x = pad_to_multiple(x, s, dim=-2)
 
         if exists(mask):
-            padded_mask = pad_to_multiple(mask, s, dim = -1, value = False)
+            padded_mask = pad_to_multiple(mask, s, dim=-1, value=False)
 
         # save the residual, and for "attention resampling" at downsample and upsample
 
@@ -268,17 +299,18 @@ class HourglassTransformer(nn.Module):
 
         if self.causal:
             shift = s - 1
-            x = F.pad(x, (0, 0, shift, -shift), value = 0.)
+            x = F.pad(x, (0, 0, shift, -shift), value=0.)
 
             if exists(mask):
-                padded_mask = F.pad(padded_mask, (shift, -shift), value = False)
+                padded_mask = F.pad(padded_mask, (shift, -shift), value=False)
 
         # naive average pool
 
         downsampled = self.downsample(x)
 
         if exists(mask):
-            downsampled_mask = reduce(padded_mask, 'b (n s) -> b n', 'sum', s = s) > 0
+            downsampled_mask = reduce(padded_mask, 'b (n s) -> b n', 'sum',
+                                      s=s) > 0
         else:
             downsampled_mask = None
 
@@ -286,21 +318,22 @@ class HourglassTransformer(nn.Module):
 
         if exists(self.attn_resampling_pre_valley):
             if exists(mask):
-                attn_resampling_mask = rearrange(padded_mask, 'b (n s) -> (b n) s', s = s)
+                attn_resampling_mask = rearrange(padded_mask,
+                                                 'b (n s) -> (b n) s', s=s)
             else:
                 attn_resampling_mask = None
 
             downsampled = self.attn_resampling_pre_valley(
                 rearrange(downsampled, 'b n d -> (b n) () d'),
-                rearrange(x, 'b (n s) d -> (b n) s d', s = s),
-                mask = attn_resampling_mask
+                rearrange(x, 'b (n s) d -> (b n) s d', s=s),
+                mask=attn_resampling_mask
             )
 
-            downsampled = rearrange(downsampled, '(b n) () d -> b n d', b = b)
+            downsampled = rearrange(downsampled, '(b n) () d -> b n d', b=b)
 
         # the "valley" - either a regular transformer or another hourglass
 
-        x = self.valley_transformer(downsampled, mask = downsampled_mask)
+        x = self.valley_transformer(downsampled, mask=downsampled_mask)
 
         valley_out = x.clone()
 
@@ -316,11 +349,11 @@ class HourglassTransformer(nn.Module):
 
         if exists(self.attn_resampling_post_valley):
             x = self.attn_resampling_post_valley(
-                rearrange(x, 'b (n s) d -> (b n) s d', s = s),
+                rearrange(x, 'b (n s) d -> (b n) s d', s=s),
                 rearrange(valley_out, 'b n d -> (b n) () d')
             )
 
-            x = rearrange(x, '(b n) s d -> b (n s) d', b = b)
+            x = rearrange(x, '(b n) s d -> b (n s) d', b=b)
 
         # bring sequence back to original length, if it were padded for pooling
 
@@ -328,25 +361,26 @@ class HourglassTransformer(nn.Module):
 
         # post-valley transformers
 
-        x = self.post_transformer(x, mask = mask)
+        x = self.post_transformer(x, mask=mask)
         return self.norm_out(x)
+
 
 # main class
 
 class HourglassTransformerLM(nn.Module):
     def __init__(
-        self,
-        *,
-        num_tokens,
-        dim,
-        max_seq_len,
-        depth,
-        shorten_factor = None,
-        heads = 8,
-        dim_head = 64,
-        attn_resampling = True,
-        updown_sample_type = 'naive',
-        causal = True
+            self,
+            *,
+            num_tokens,
+            dim,
+            max_seq_len,
+            depth,
+            shorten_factor=None,
+            heads=8,
+            dim_head=64,
+            attn_resampling=True,
+            updown_sample_type='naive',
+            causal=True
     ):
         super().__init__()
         self.max_seq_len = max_seq_len
@@ -355,24 +389,24 @@ class HourglassTransformerLM(nn.Module):
         self.pos_emb = nn.Embedding(max_seq_len, dim)
 
         self.transformer = get_hourglass_transformer(
-            dim = dim,
-            depth = depth,
-            shorten_factor = shorten_factor,
-            attn_resampling = attn_resampling,
-            updown_sample_type = updown_sample_type,
-            dim_head = dim_head,
-            heads = heads,
-            causal = causal,
-            norm_out = True
+            dim=dim,
+            depth=depth,
+            shorten_factor=shorten_factor,
+            attn_resampling=attn_resampling,
+            updown_sample_type=updown_sample_type,
+            dim_head=dim_head,
+            heads=heads,
+            causal=causal,
+            norm_out=True
         )
 
         self.to_logits = nn.Linear(dim, num_tokens)
 
-    def forward(self, x, mask = None):
+    def forward(self, x, mask=None):
         device = x.device
         x = self.token_emb(x)
-        pos_emb = self.pos_emb(torch.arange(x.shape[-2], device = device))
+        pos_emb = self.pos_emb(torch.arange(x.shape[-2], device=device))
         x = x + rearrange(pos_emb, 'n d -> () n d')
 
-        x = self.transformer(x, mask = mask)
+        x = self.transformer(x, mask=mask)
         return self.to_logits(x)
